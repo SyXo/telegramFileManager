caa959355e6ad3dfc1764c6dfaa0976a76e54a70
Nicu Borta <nicuborta123@tutanota.com>
2020-05-30 19:55:32 +0300

    main.py: Add basic curses interface

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import curses
 import pyrCaller
 import configparser
-from os import path
+import os
 
 cfg = configparser.ConfigParser()
-cfg.read(path.expanduser("~/.config/tgFileManager.ini"))
+cfg.read(os.path.expanduser("~/.config/tgFileManager.ini"))
 
+'''
 print("{}\n{}\n{}\n{}\n{}".format(
       cfg['telegram']['api_id'],
       cfg['telegram']['api_hash'],
@@ -13,3 +14,37 @@ print("{}\n{}\n{}\n{}\n{}".format(
       cfg['paths']['data_path'],
       cfg['paths']['tmp_path']
 ))
+'''
+
+MAX_SESSIONS = int(cfg['telegram']['max_sessions'])
+usedSessions = [1] # list of which sessions are used
+
+# initialize the screen
+scr = curses.initscr()
+
+curses.noecho()
+curses.cbreak()
+curses.curs_set(0)
+scr.keypad(1)
+
+try:
+    while True:
+        scr.clear()
+        tlX, tlY = os.get_terminal_size(0)
+
+        usedSessionStr = "[ {} of {} ]".format(len(usedSessions), MAX_SESSIONS)
+
+        scr.addstr(1, max(tlX-len(usedSessionStr), 0),
+                   usedSessionStr, curses.A_NORMAL)
+
+        ch = scr.getch()
+        if ch == 17: # Ctrl+Q
+            break
+except KeyboardInterrupt:
+    pass
+
+# exit
+scr.keypad(0)
+curses.echo()
+curses.nocbreak()
+curses.endwin()
